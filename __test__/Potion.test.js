3b3c72c8d462f0b0d606cfac7c0120152e9e9f37
Carolina Reedy <carinvid@gmail.com>
2021-07-07 21:53:54 -0600

    adding code to poison.js

--- __test__/Potion.test.js
+++ __test__/Potion.test.js
@@ -1,6 +1,9 @@
-test("creates a health potion object", () => {
-  const potion = new Potion("health");
+const Potion = require("../lib/Potion.js");
 
-  expect(potion.name).toBe("health");
+test("creates a random potion object", () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
 });
