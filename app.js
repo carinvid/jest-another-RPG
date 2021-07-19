300a4927bec900913afcbec54266ce10c1e741e0
Carolina Reedy <carinvid@gmail.com>
2021-07-18 23:42:31 -0600

    adding files

---
+++ app.js
@@ -0,0 +1,29 @@
+const Game = require("./lib/Game");
+
+new Game().initializeGame();
+inquirer
+  .prompt({
+    type: "text",
+    name: "name",
+    message: "What is your name?",
+  })
+  // destructure name from the prompt object
+  .then(({ name }) => {
+    this.player = new Player(name);
+
+    // test the object creation
+    console.log(this.currentEnemy, this.player);
+  })
+
+  .then(function ({ name }) {
+    this.player = new Player(name);
+
+    // test the object creation
+    console.log(this.currentEnemy, this.player);
+  })
+
+  .then(({ name }) => {
+    this.player = new Player(name);
+
+    this.startNewBattle();
+  });
